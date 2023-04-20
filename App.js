0cc71e704f85c7b80cbb3cf6a221e61841664777
AliinaStar <128737579+AliinaStar@users.noreply.github.com>
2023-04-20 19:09:41 +0300

    Added react-native-paper

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper'
+import {Appbar} from './components'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <PaperProvider>
+    <View>
+      <Appbar />
+      <Text>Welcome to MyMenu!</Text>
       <StatusBar style="auto" />
     </View>
+    </PaperProvider>
   );
 }
 
