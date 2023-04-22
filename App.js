3631534288fc17383928c767f9c5a65a5b8b367e
AliinaStar <128737579+AliinaStar@users.noreply.github.com>
2023-04-22 14:40:43 +0300

    change App

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
     <View>
       <Appbar />
       <Text>Welcome to MyMenu!!!</Text>
-      <Button.region />
+      <Buttonregion />
       <StatusBar style="auto" />
     </View>
     </PaperProvider>
